5c47e0923260d3ea3d6e536af946a1d1b9e7a793
Sunder <jogisunder26@gmail.com>
2019-10-11 21:25:14 +0530

    new commits at 11/10

--- src/main/java/com/hospital/controller/repository/DoctorRepository.java
+++ src/main/java/com/hospital/controller/repository/DoctorRepository.java
@@ -1,9 +1,8 @@
 package com.hospital.controller.repository;
 
-import com.hospital.controller.model.Department;
 import com.hospital.controller.model.Doctor;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.hateoas.alps.Doc;
+
 
 
 public interface DoctorRepository extends JpaRepository<Doctor, Long> {
